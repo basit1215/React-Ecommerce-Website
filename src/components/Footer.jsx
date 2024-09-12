02d7a565e4d96b86e8f925d12e8e36d3933757e1
Abdul Basit <154369335+basit1215@users.noreply.github.com>
2024-09-12 21:57:28 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
 
         <div className='fixed bottom-0 left-0 w-full bg-black text-white p-6 text-lg sm:text-base md:text-sm lg:text-xs text-center'>
-            <p>© 2024 Basit Blog. All rights reserved.</p>
+            <p> © Abdul Basit | All Rights Reserved </p>
         </div>
 
 
